037e6c688bccd7a66cb5673ad60612073bf717db
Javier Carreño <carrenojavier015@gmail.com>
2020-07-24 20:29:58 -0500

    Add all my files

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {addUser,removeUser,getUser,getUsersInRoom} = require('./user');
+
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
+const { createSocket } = require('dgram');
+//const { default: symbolicateStackTrace } = require('react-native/Libraries/Core/Devtools/symbolicateStackTrace');
 
 
 const app = express();
@@ -14,6 +18,22 @@ const io = socketio(server);
 io.on('connection',(socket) => {
     
     console.log('we have a new connection!!!')
+
+    socket.on('join',({name,room},callback)=> {
+
+        const {error,user} = addUser({id:socket.id,name,room});
+        if(error) return callback(error)
+        //having in the frond end because is emiting the massage
+        socket.emit('message',{user:'admin',text:`${user.name},Welcome to the room ${user.room}`})
+        createSocket.broadcas.to(user.room).emit('messager',{user: 'admin',text: `${user.name},has joing`})
+        socket.join(user.room);
+        callback();
+    });
+    socket.on('sendMessager',(message,callback)=> {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message',{user: user.name, text: message})
+        callback();
+    })
     socket.on('disconnect',() => {
         console.log('user has gone out of the chat!!!');
     });
