23cd20cb5081ec738797e2796a6e0288a5962313
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-09-01 20:54:46 -0700

    Add files via upload

--- server/user.js
+++ server/user.js
@@ -1,40 +1,32 @@
-const users=[];
+const users = [];
 
-const addUser = (id,name,room) => {
-    //javaScript = user 
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    name = name.trim().toLowerCase();
-    room = room.trim().toLowerCase();
+  const existingUser = users.find((user) => user.room === room && user.name === name);
 
-    const existingUser = users.find((user) => user.room === room && user.name === name);
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
 
-    if(existingUser){
-        return ('error: taken for the user');
-    }
+  const user = { id, name, room };
 
-    const user = {id,name,room};
-    user.push(user);
+  users.push(user);
 
+  return { user };
 }
 
-const removeUser = (id) =>{
-    const index = users.findIndex((user) => user.id === id);
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
 
   if(index !== -1) return users.splice(index, 1)[0];
-    
-
-     
 }
 
-const getUser = (id) => users.find((user) => user.id === id)
+const getUser = (id) => users.find((user) => user.id === id);
 
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-
-
-
-const getUsersInRoom = (room) => room.find((user) => user.room === room)
-
-module.export = {addUser,removeUser,getUser,getUsersInRoom}
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
 
 
 
